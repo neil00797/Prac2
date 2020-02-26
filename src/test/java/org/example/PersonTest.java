d0f29e8bcf54b27b067a93112732edc99782ba2a
Brandon-Villet <villetbrandon@gmail.com>
2020-02-26 10:32:54 +0200

    First commit

---
+++ src/test/java/org/example/PersonTest.java
@@ -0,0 +1,55 @@
+package org.example;
+
+
+import org.junit.*;
+
+public class PersonTest {
+
+    Person Neil, Brandon, Strauss;
+
+    @Before
+    public void setUp(){
+        Neil = new Person("Neil");
+
+        Brandon= new Person("Neil");
+
+        Strauss = Neil;
+    }
+
+
+
+    @After
+    public void tearDown(){}
+
+
+    @Test
+    public void testIdentity(){
+        System.out.println(Neil.hashCode());
+        System.out.println(Strauss.hashCode());
+
+        Assert.assertTrue(Strauss==Neil);
+    }
+
+    @Test
+    public void testEquality(){
+        System.out.println(Neil.toString());
+        System.out.println(Brandon.toString());
+
+        Assert.assertTrue(Neil==Brandon);
+    }
+
+    @Test(timeout = 3000)
+    public void testTimeout(){
+        while (true){
+
+        }
+    }
+
+    @Ignore
+    @Test
+    public void testIgbore(){
+        while (true){
+
+        }
+    }
+}
